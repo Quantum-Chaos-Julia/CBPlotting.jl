697419d6eba2bdb69b2de337bf5568b03a22df91
Črt Lozej <clozej@gmail.com>
2025-04-29 18:34:42 +0200

    added full domain plotting

--- src/trajectory.jl
+++ src/trajectory.jl
@@ -1,7 +1,7 @@
 #trajectory plotting
 function plot_trajectory!(ax, particle, billiard, T; dt = 0.01, full_domain=false, traj_args = traj_args, plot_velocity = false)
     pts, vel, ts = trajectory(particle, billiard, T; dt, full_domain)
-    plot_domain!(ax,billiard, fundamental_only=true)
+    plot_domain!(ax,billiard, fundamental_only=!full_domain)
     lines!(ax, getindex.(pts,1), getindex.(pts,2); traj_args...)
     if plot_velocity
         arrows!(ax, getindex.(pts,1), getindex.(pts,2), getindex.(vel,1), getindex.(vel,2); lengthscale = 0.05)
