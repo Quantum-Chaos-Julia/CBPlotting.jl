76301e15eb217116c6971af7ac41d2ffcf7f0ed0
Črt Lozej <clozej@gmail.com>
2025-04-29 10:25:49 +0200

    added trajectory plotting

--- src/CBPlotting.jl
+++ src/CBPlotting.jl
@@ -8,6 +8,8 @@ using ClassicalBilliards
 
 using StaticArrays
 
+traj_args = Dict(:linewidth=>0.75, :linestyle=>:solid, :transparency => true, :alpha => 0.1)
 include("trajectory.jl")
+
 
 end
---
+++ src/trajectory.jl
@@ -0,0 +1,11 @@
+#trajectory plotting
+function plot_trajectory!(ax, particle, billiard, T; dt = 0.01, full_domain=false, traj_args = traj_args, plot_velocity = false)
+    pts, vel, ts = trajectory(particle, billiard, T; dt, full_domain)
+    plot_domain!(ax,billiard, fundamental_only=true)
+    lines!(ax, getindex.(pts,1), getindex.(pts,2); traj_args...)
+    if plot_velocity
+        arrows!(ax, getindex.(pts,1), getindex.(pts,2), getindex.(vel,1), getindex.(vel,2); lengthscale = 0.05)
+    end
+end
+
+export plot_trajectory!
